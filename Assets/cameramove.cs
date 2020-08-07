da91435c17f1023cb18353b90a7cc9a21307c168
afifnuril17 <afif.nuril17@gmail.com>
2020-08-07 09:33:29 +0700

    berjalan2d

---
+++ Assets/cameramove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class cameramove : MonoBehaviour, IDragHandler
+{
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.position = Input.mousePosition * 1f;
+        }
+    }
+}
---
+++ Assets/dragjalan.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class dragjalan : MonoBehaviour
+{ 
+    public Transform player;
+    public float distanceFromCamera;
+    public Camera cam;
+    Rigidbody2D r;
+
+   
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceFromCamera = Vector3.Distance(player.position, cam.transform.position);
+
+        r = player.GetComponent<Rigidbody2D>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Input.mousePosition - player.transform.position;
+        pos.z = distanceFromCamera;
+
+        pos = cam.ScreenToWorldPoint(pos);
+        //player.position = pos;
+
+        r.velocity = (pos - player.transform.position) * 5;
+        //player.transform.position = Input.mousePosition;
+
+    }
+}
